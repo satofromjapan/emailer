586dc18ae5fcf48512439bf2bbd463c1931df056
Masato Takahashi <masato.takaha@gmail.com>
2017-11-27 14:23:27 -0800

    Added billinmg and client side app

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
 	cookieSession({
@@ -21,6 +24,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	//Express will serve up production assets like main.js or main.css file
+	app.use(express.static('client/build'));
+
+	// Express will serve up the index.html file if it doesn't recognize the route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(--dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
